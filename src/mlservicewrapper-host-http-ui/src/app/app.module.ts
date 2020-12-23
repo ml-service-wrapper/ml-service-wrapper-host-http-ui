592777a0965a35668037215d771f237f536708ad
Matthew Haugen <8117615+MaJaHa95@users.noreply.github.com>
2020-12-23 02:23:40 -0800

    Remove routing

--- src/mlservicewrapper-host-http-ui/src/app/app.module.ts
+++ src/mlservicewrapper-host-http-ui/src/app/app.module.ts
@@ -4,20 +4,15 @@ import { FormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxFileDropModule } from "ngx-file-drop";
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProcessPageComponent } from './Pages/process-page/process-page.component';
-
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProcessPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     NgxFileDropModule,
     NgbModule,
